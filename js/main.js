9ac2323201e1b7ffa87966e7b42a10ca3955edb1
JarSeal <kai.forsman@gmail.com>
2023-12-04 15:03:13 +0100

    Publish the site

--- js/main.js
+++ js/main.js
@@ -1,63 +1,96 @@
 // APP DATA
 const data = [
-  // {
-  //   id: "hero1",
-  //   name: "",
-  //   superpower: "",
-  //   image: ""
-  // },
+  {
+    id: "hero1",
+    name: "Cheese-man!",
+    superpower: "It stinks!",
+    weakness: "People!",
+    image: "https://th.bing.com/th/id/OIG.oVm9pILUCKlNw50XsEVj?w=1024&h=1024&rs=1&pid=ImgDetMain"
+  },
+  {
+    id: "hero2",
+    name: "Gianna the Superhero",
+    superpower: "Being cool!",
+    weakness: "Afraid of Jimmy",
+    image: "https://th.bing.com/th/id/OIG.2_lI0Wc1SRDGW5ejtzQ.?pid=ImgGn"
+  },
+  {
+    id: "hero3",
+    name: "Cupcake man",
+    superpower: "Cupcakes",
+    weakness: "Cupcakes",
+    image: "https://th.bing.com/th/id/OIG.8WALtYkrUq7kVYRgm0VQ?pid=ImgGn"
+  },
+  {
+    id: "hero4",
+    name: "Immune man",
+    superpower: "Not killable, healthy, fly, superspeed, invisibility",
+    weakness: "Sweets, not healthy",
+  },
+  {
+    id: "hero5",
+    name: "Loud Whisperer",
+    superpower: "Whispers really loud",
+    image: "https://th.bing.com/th/id/OIG._Gx1ghcSzivTI5k.N1nE?w=1024&h=1024&rs=1&pid=ImgDetMain"
+  },
+  {
+    id: "hero6",
+    name: "Chair Breaker",
+    superpower: "Breaks a lot of chairs",
+    image: "https://th.bing.com/th/id/OIG.T7.UgM3ELbTcuX4UneJs?pid=ImgGn"
+  }
 ]
 
 // HOME PAGE
 const homePage = function() {
-  // const cardsWrapperElem = document.getElementById('heroes');
-  // data.forEach(function(card) {
-  //   const cardToBeAdded = document.createElement('button');
-  //   cardToBeAdded.classList.add('card');
+  const cardsWrapperElem = document.getElementById('heroes');
+  data.forEach(function(card) {
+    const cardToBeAdded = document.createElement('button');
+    cardToBeAdded.classList.add('card');
 
-  //   if (card.image) {
-  //     cardToBeAdded.innerHTML = `
-  //       <h2 class="name">${card.name}</h2>
-  //       <div class="imageWrapper">
-  //         <img src="${card.image}" />
-  //       </div>
-  //   `;
-  //   } else {
-  //     cardToBeAdded.innerHTML = `
-  //       <h2 class="name">${card.name}</h2>
-  //       <div class="imageWrapper"></div>
-  //     `;
-  //   }
+    if (card.image) {
+      cardToBeAdded.innerHTML = `
+        <h2 class="name">${card.name}</h2>
+        <div class="imageWrapper">
+          <img src="${card.image}" />
+        </div>
+    `;
+    } else {
+      cardToBeAdded.innerHTML = `
+        <h2 class="name">${card.name}</h2>
+        <div class="imageWrapper"></div>
+      `;
+    }
 
-  //   cardsWrapperElem.append(cardToBeAdded);
+    cardsWrapperElem.append(cardToBeAdded);
 
-  //   cardToBeAdded.addEventListener('click', function() {
-  //     window.location.href = `hero.html?id=${card.id}`;
-  //   })
-  // });
+    cardToBeAdded.addEventListener('click', function() {
+      window.location.href = `hero.html?id=${card.id}`;
+    })
+  });
 }
 
 // CARD PAGE
 const cardPage = function() {
-  // const params = new URLSearchParams(location.search);
-  // const heroId = params.get('id');
-  // const heroData = data.find(function(hero) {
-  //   if (hero.id === heroId) {
-  //     return hero;
-  //   }
-  // })
+  const params = new URLSearchParams(location.search);
+  const heroId = params.get('id');
+  const heroData = data.find(function(hero) {
+    if (hero.id === heroId) {
+      return hero;
+    }
+  })
   
-  // const nameElem = document.getElementById('name');
-  // const superpowerElem = document.getElementById('superpower');
-  // const bigImageElem = document.getElementById('bigImage');
+  const nameElem = document.getElementById('name');
+  const superpowerElem = document.getElementById('superpower');
+  const bigImageElem = document.getElementById('bigImage');
 
-  // nameElem.textContent = heroData.name;
-  // superpowerElem.textContent = heroData.superpower;
-  // if (heroData.image) {
-  //   bigImageElem.src = heroData.image;
-  // } else {
-  //   bigImageElem.parentElement.textContent = '(No image found)';
-  // }
+  nameElem.textContent = heroData.name;
+  superpowerElem.textContent = heroData.superpower;
+  if (heroData.image) {
+    bigImageElem.src = heroData.image;
+  } else {
+    bigImageElem.parentElement.textContent = '(No image found)';
+  }
 }
 
 // CHECK WHICH PAGE
@@ -78,18 +111,3 @@ if (PAGE === 'home') {
 
 
 
-
-
-
-// {
-  //   id: "hero1",
-  //   name: "Loud Whisperer",
-  //   superpower: "Whispers really loud",
-  //   image: "https://th.bing.com/th/id/OIG._Gx1ghcSzivTI5k.N1nE?w=1024&h=1024&rs=1&pid=ImgDetMain"
-  // },
-  // {
-  //   id: "hero2",
-  //   name: "Chair Breaker",
-  //   superpower: "Breaks a lot of chairs",
-  //   image: "https://th.bing.com/th/id/OIG.T7.UgM3ELbTcuX4UneJs?pid=ImgGn"
-  // }
